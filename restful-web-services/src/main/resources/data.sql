8fb7e69f70083a421fa3c591966ae2b32fcf6067
Alex Nahayo <alexn794@gmail.com>
2023-05-01 20:17:34 +0100

    adding POST, PUT DELETE Methods to todJpaResource Class and TodoDataService classes

---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo(id, username, description, target_date, is_done)
+values (10001,'in28minutes','Learn JPA', CURRENT_TIMESTAMP, 0 );
+
+insert into todo(id, username, description, target_date, is_done)
+values (10002,'in28minutes','Learn Date JPA', CURRENT_TIMESTAMP, 0 );
+
+insert into todo(id, username, description, target_date, is_done)
+values (10003,'in28minutes','Learn Microservices', CURRENT_TIMESTAMP, 0 );
